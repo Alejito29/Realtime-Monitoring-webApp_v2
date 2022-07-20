64ff3a5f8bc15d574bb823a97da2f2edc2061f9c
Wilson Gonzalez <wilson1234567891962@hotmail.com>
2022-07-20 10:19:23 -0500

    Update

--- IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.226.160.67",  # Dirección IP de la base de datos
+        "HOST": "54.90.176.137",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -157,7 +157,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.167.6.42"
+MQTT_HOST = "54.91.39.161"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
